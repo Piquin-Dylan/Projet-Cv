8f7bf8837da79de44c04b911873ff97636e42194
Piquin-Dylan <dylan.piquin@student.hepl.be>
2025-03-10 16:54:44 +0100

    header + conversion en em

--- javascript/main.js
+++ javascript/main.js
@@ -2,16 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll(".container_list a"); // Sélection des liens
     const contents = document.querySelectorAll(".profil.content"); // Sélection des sections
 
-    // Fonction pour cacher tous les articles
     function hideAllContents() {
         contents.forEach(content => content.style.display = "none");
     }
 
-    // Cacher tous les articles sauf le premier au chargement
     hideAllContents();
     document.querySelector("#item_1").style.display = "block";
 
-    // Gestion du clic sur les liens
     links.forEach(link => {
         link.addEventListener("click", function (event) {
             event.preventDefault(); // Supprime l'effet d'ancre
@@ -19,9 +16,52 @@ document.addEventListener("DOMContentLoaded", function () {
             const targetElement = document.querySelector(targetId);
 
             if (targetElement) {
-                hideAllContents(); // Cache tous les articles
-                targetElement.style.display = "block"; // Affiche le bon article
+                hideAllContents();
+                targetElement.style.display = "block";
             }
         });
+    });
+});
+const arrow = document.querySelectorAll('.arrow')
+const articleContent = document.querySelectorAll('.article article')
+const info = document.querySelector('.info')
+let currentIndex = 0
+console.log(currentIndex)
+arrow.forEach(arrows => {
+    arrows.addEventListener('click', (evt) => {
+        evt.preventDefault()
+        articleContent.forEach(article => {
+            article.style.display = 'none'
+
+            if (evt.target.classList.contains('arrow_left')) {
+                articleContent[currentIndex].style.display = 'block'
+                currentIndex--
+                console.log(currentIndex)
+            }
+
+            if (evt.target.classList.contains('arrow_right')) {
+                currentIndex++
+                console.log(currentIndex)
+            }
+            if (currentIndex === 3) {
+                currentIndex = 0
+            }
+            // Si currentIndex dépasse la limite (dernier article), revenir au premier
+            if (currentIndex === articleContent.length) {
+                currentIndex = 0;
+
+            }
+            // Si currentIndex devient négatif, revenir au dernier article
+            if (currentIndex < 0) {
+                currentIndex = articleContent.length - 1;
+            }
+        })
+
+    })
+})
+document.querySelectorAll('.navigation__secondaire').forEach(item => {
+    item.addEventListener('click', function (event) {
+alert('bjr')
+        event.preventDefault()
     });
 });
